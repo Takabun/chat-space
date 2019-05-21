d6620f8dbcad26782ba3baa05e993f10d1cbfdd1
Takabun <takafumi.kido@gmail.com>
2019-05-21 20:50:09 +0900

    Error: Unexpected character '`'を修正！

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,30 +4,30 @@ $(function() {
   
   function appendUserToSearchList(user){
     var html = 
-    `<div class="chat-group-user clearfix">
+    <div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${ user.name }</p>
       <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
-    </div>`
+    </div>
     search_list.append(html);
     return html;
   }
 
   function appendNoUserToSearchList(user) {
     var html = 
-      `<div class="chat-group-user clearfix">
+      <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${ user }</p>
-      </div>`
+      </div>
       search_list.append(html);
       return html;
     }
     
     function appendUserToMemberList(name, user_id) {
       var html = 
-        `<div class='chat-group-user clearfix js-chat-member'>
+        <div class='chat-group-user clearfix js-chat-member'>
           <input name='group[user_ids][]' type='hidden' value=${ user_id }>
           <p class='chat-group-user__name'>${ name }</p>
           <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-        </div>`
+        </div>
   　　 member_list.append(html);
       return html;
     }
